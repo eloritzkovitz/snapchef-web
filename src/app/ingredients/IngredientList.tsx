8bbce67858d547a4640c37a4852487071fe2a038
Elor-Itz <elor2200@gmail.com>
2025-05-02 18:47:58 +0300

    Added icons

--- src/app/ingredients/IngredientList.tsx
+++ src/app/ingredients/IngredientList.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState, useRef } from "react";
 import Link from "next/link";
+import { ArrowLeftIcon, ArrowPathIcon, ChevronDownIcon, XMarkIcon, MagnifyingGlassIcon, PlusIcon, PencilIcon, TrashIcon } from "@heroicons/react/24/solid";
 import categories from "../../data/categories";
 import AddIngredientForm from "./AddIngredientForm";
 import EditIngredientForm from "./EditIngredientForm";
@@ -34,7 +35,9 @@ const IngredientList: React.FC = () => {
   // Fetch all ingredients from the API
   const fetchIngredients = async () => {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/ingredients`);
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/ingredients`
+      );
       if (!response.ok) {
         throw new Error("Failed to fetch ingredients.");
       }
@@ -148,46 +151,59 @@ const IngredientList: React.FC = () => {
     );
     setIsEditModalOpen(false);
     setIngredientToEdit(null);
-  };  
+  };
 
   return (
     <div className="p-6 bg-white shadow-md rounded-lg">
       <div className="flex items-center mb-4">
         {/* Header */}
         <Link href="/" className="mr-2 text-black flex items-center">
-          <span className="text-lg">←</span>
+          <ArrowLeftIcon className="h-5 w-5" />
         </Link>
         <h1 className="text-2xl font-bold">View Ingredients</h1>
       </div>
-      
+
       {/* Menu */}
       <div className="flex justify-between items-center mb-4">
-        <input
-          type="text"
-          placeholder="Search ingredients..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full max-w-md p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <select
-          value={selectedCategory}
-          onChange={(e) => setSelectedCategory(e.target.value)}
-          className="ml-4 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          <option value="">All Categories</option>
-          {categories.map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
+        {/* Search */}
+        <div className="flex items-center w-full max-w-md border border-gray-300 rounded-md focus-within:ring-2 focus-within:ring-blue-500">
+          <MagnifyingGlassIcon className="h-5 w-5 text-gray-500 ml-2" />
+          <input
+            type="text"
+            placeholder="Search ingredients..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full p-2 focus:outline-none"
+          />
+        </div>
+
+        {/* Select category */}
+        <div className="relative">
+          <select
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+            className="appearance-none px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option value="">All Categories</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          <ChevronDownIcon className="absolute right-2 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500 pointer-events-none" />
+        </div>
+
+        {/* Add Ingredient */}
         <button
           onClick={() => setIsAddModalOpen(true)}
-          className="ml-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+          className="ml-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 flex items-center gap-1"
         >
-          Add Ingredient
+          <PlusIcon className="h-5 w-5" /> Add Ingredient
         </button>
       </div>
+
+      {/* Table */}
       <div className="overflow-x-auto">
         <table className="w-full border-collapse border border-gray-200 table-fixed">
           <thead>
@@ -224,18 +240,18 @@ const IngredientList: React.FC = () => {
                       setIngredientToEdit(ingredient);
                       setIsEditModalOpen(true);
                     }}
-                    className="px-2 py-1 bg-yellow-500 text-white rounded-md hover:bg-yellow-600"
+                    className="px-2 py-1 bg-yellow-500 text-white rounded-md hover:bg-yellow-600 flex items-center gap-1"
                   >
-                    Edit
+                    <PencilIcon className="h-5 w-5" /> Edit
                   </button>
                   <button
                     onClick={() => {
                       setIngredientToDelete(ingredient);
                       setIsDeleteDialogOpen(true);
                     }}
-                    className="px-2 py-1 bg-red-500 text-white rounded-md hover:bg-red-600"
+                    className="px-2 py-1 bg-red-500 text-white rounded-md hover:bg-red-600 flex items-center gap-1"
                   >
-                    Delete
+                    <TrashIcon className="h-5 w-5" /> Delete
                   </button>
                 </td>
               </tr>
@@ -245,14 +261,23 @@ const IngredientList: React.FC = () => {
       </div>
 
       {page * ITEMS_PER_PAGE < filteredIngredients.length && (
-        <div ref={loaderRef} className="text-center py-4">
-          <p>Loading more ingredients...</p>
+        <div
+          ref={loaderRef}
+          className="text-center py-4 flex items-center justify-center"
+        >
+          <ArrowPathIcon className="animate-spin h-5 w-5 text-black mr-2" />
         </div>
       )}
 
       {isAddModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-transparent">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative">
+            <button
+              onClick={() => setIsAddModalOpen(false)}
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 cursor-pointer"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
             <AddIngredientForm
               categories={categories}
               onAddIngredient={handleAddIngredient}
@@ -264,7 +289,15 @@ const IngredientList: React.FC = () => {
 
       {isEditModalOpen && ingredientToEdit && (
         <div className="fixed inset-0 flex items-center justify-center bg-transparent">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative">
+            {/* Close Button */}
+            <button
+              onClick={() => setIsEditModalOpen(false)}
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 cursor-pointer"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+            {/* Edit Ingredient Form */}
             <EditIngredientForm
               categories={categories}
               ingredient={ingredientToEdit}
