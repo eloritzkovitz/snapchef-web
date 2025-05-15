49a927b89bfed95874d1f4c13da2174f6109ae9a
Elor-Itz <elor2200@gmail.com>
2025-05-15 20:05:06 +0300

    Redesigned navbar and favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,25 +2,39 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 text-white p-4">
+    <nav className="bg-[#f47851] text-white p-4 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
         {/* Navbar Banner */}
-        <Link href="/" className="text-xl font-bold hover:underline">
-          SnapChef
+        <Link href="/" className="flex items-center space-x-3">
+          <img
+            src="/icon_white.png"
+            alt="SnapChef Logo"
+            className="h-12 w-12"
+          />
+          <span className="text-4xl font-bold">SnapChef</span>
         </Link>
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-6">
           <li>
-            <Link href="/api" className="hover:underline">
+            <Link
+              href="/api"
+              className="hover:text-[#ffb89d] transition-colors"
+            >
               API
             </Link>
           </li>
           <li>
-            <Link href="/about" className="hover:underline">
+            <Link
+              href="/about"
+              className="hover:text-[#ffb89d] transition-colors"
+            >
               About
             </Link>
           </li>
           <li>
-            <Link href="/login" className="hover:underline">
+            <Link
+              href="/login"
+              className="hover:text-[#ffb89d] transition-colors"
+            >
               Log In
             </Link>
           </li>
