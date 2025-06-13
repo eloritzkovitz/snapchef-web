0e0c640387f0ffce9859f78e5909c5381f185d9a
Elor-Itz <elor2200@gmail.com>
2025-06-13 13:49:57 +0300

    Fixed scrolling

--- src/app/api/page.tsx
+++ src/app/api/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 export default function ApiDocsPage() {
-  
   return (
-    <div style={{ height: "100vh", width: "100%", background: "#fff" }}>
-      <iframe
-        src="https://snapchef.cs.colman.ac.il/api-docs/"
-        title="API Documentation"
-        style={{ width: "100%", height: "100%", border: "none" }}
-        allowFullScreen
-      />
-    </div>
+    <iframe
+      src="https://snapchef.cs.colman.ac.il/api-docs/"
+      title="API Documentation"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        display: "block",
+      }}
+      allowFullScreen
+    />
   );
 }
